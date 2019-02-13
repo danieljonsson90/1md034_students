53c634a30fd3ec7d7bfcddf782e8dafa9a068ad2
danieljonsson90 <Daniel.Jonsson.1320@student.uu.se>
2019-02-13 14:31:31 +0100

    map går att klicka

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,4 +1,7 @@
 
+'use strict';
+var socket = io();
+
 function MenuItem(name,kCal,g,l,img) {
     this.burgername = name;
     this.Calories = kCal;
@@ -9,27 +12,49 @@ function MenuItem(name,kCal,g,l,img) {
         return this.burgername;
         }; 
       }
-
 var vm = new Vue ({
+
     el:'#mainId',
     data: {
         food: items,
         Fullname: '',
         Email: '',
-        Street: '',
-        House: '',
         Payment: '',
         Gender:'Other',
         Choise: [],
+        orders: {},
         form: null
     },
+    created: function () {
+        socket.on('initialize', function (data) {
+          this.orders = data.orders;
+        }.bind(this));
+   
+        socket.on('currentQueue', function (data) {
+          this.orders = data.orders;
+        }.bind(this));
+      },
     methods: {
         updateFunction: function() {
-            this.form = ['Your Orders: ' + this.Choise,'Fullname: '+ this.Fullname,'Email adress: ' + this.Email,'Street: ' + this.Street,'House number: ' +  this.House,'Payment: ' 
+            this.form = ['Your Orders: ' + this.Choise,'Fullname: '+ this.Fullname,'Email adress: ' + this.Email,'Payment: ' 
             + this.Payment,'Gender: ' + this.Gender];
             console.log("check");
-        }
+        },
+        getNext: function () {
+            var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
+              return Math.max(last, next);
+            }, 0);
+            return lastOrder + 1;
+          },
+          addOrder: function (event) {
+            var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                          y: event.currentTarget.getBoundingClientRect().top};
+            socket.emit("addOrder", { orderId: this.getNext(),
+                                      details: { x: event.clientX - 10 - offset.x,
+                                                 y: event.clientY - 10 - offset.y },
+                                      orderItems: ["Beans", "Curry"]
+                                    });
+          }
     }
 })
- 
 
