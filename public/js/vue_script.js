c1692f55d8cd2cd375ec0e241c3770d4b7871cf2
danieljonsson90 <Daniel.Jonsson.1320@student.uu.se>
2019-02-15 15:58:20 +0100

    messaging klar

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -23,37 +23,25 @@ var vm = new Vue ({
         Gender:'Other',
         Choise: [],
         orders: {},
+        orderId: 1,
         form: null
     },
-    created: function () {
-        socket.on('initialize', function (data) {
-          this.orders = data.orders;
-        }.bind(this));
-   
-        socket.on('currentQueue', function (data) {
-          this.orders = data.orders;
-        }.bind(this));
-      },
     methods: {
-        updateFunction: function() {
-            this.form = ['Your Orders: ' + this.Choise,'Fullname: '+ this.Fullname,'Email adress: ' + this.Email,'Payment: ' 
-            + this.Payment,'Gender: ' + this.Gender];
-            console.log("check");
+        addOrder: function(event) {
+            socket.emit("addOrder", { orderId: this.orderId,
+                details: this.orders.details, orderItems: [this.Choise], personalInfo: [this.Fullname, this.Email, this.Payment, this.Gender]
+              },
+              );
+              this.orderId += 1;
+            
         },
-        getNext: function () {
-            var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
-              return Math.max(last, next);
-            }, 0);
-            return lastOrder + 1;
-          },
-          addOrder: function (event) {
+          displayOrder: function (event) {
             var offset = {x: event.currentTarget.getBoundingClientRect().left,
                           y: event.currentTarget.getBoundingClientRect().top};
-            socket.emit("addOrder", { orderId: this.getNext(),
-                                      details: { x: event.clientX - 10 - offset.x,
-                                                 y: event.clientY - 10 - offset.y },
-                                      orderItems: ["Beans", "Curry"]
-                                    });
+            this.orders = {details: { 
+                                    x: event.clientX - 10 - offset.x,
+                                    y: event.clientY - 10 - offset.y },
+                                    };
           }
     }
 })
